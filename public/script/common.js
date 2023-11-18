af0be80cce4736cfb3a3d65adb24bbe95418e66b
Salamander <salamanderhash0514@gmail.com>
2023-11-18 22:35:03 +1300

    Start redesigning website

--- public/script/common.js
+++ public/script/common.js
@@ -28,36 +28,6 @@ function areObjectsEqual(o1, o2) {
     }
 }
 
-function makeLogin(logStatus) {
-    const username = document.createElement("a");
-    username.innerText = "Log In";
-    username.href = `/public/login.html`
-
-    username.style.textDecoration = "none";
-    username.style.color = "fee";
-    username.style.display = "inline-block";
-    username.style.fontWeight = "bold";
-    username.style.fontSize = "20pt";
-
-    logStatus.appendChild(username);
-}
-
-function makeProfile(logStatus, id, password) {
-    fetch(`/api/profile?id=${id}`).then(res => res.json()).then(data => {
-        const username = document.createElement("a");
-        username.innerText = data.username;
-        username.href = `/public/profile.html?id=${id}`
-
-        username.style.textDecoration = "none";
-        username.style.color = "#fff";
-        username.style.display = "inline-block";
-        username.style.fontWeight = "bold";
-        username.style.fontSize = "20pt";
-
-        logStatus.appendChild(username);
-    });
-}
-
 function logOut() {
     console.log(document.cookie);
     document.cookie = "id=;path=/;expires=Thu, 01 Jan 1970 00:00:01 GMT"
@@ -66,28 +36,56 @@ function logOut() {
     location.reload();
 }
 
+function makeLoggedOut(menu) {
+    let login = document.createElement("a");
+    login.innerText = "Log In";
+    login.href = "/public/login.html";
+
+    menu.appendChild(login);
+}
+
+function makeLoggedIn(menu, id) {
+    fetch(`/api/profile?id=${id}`).then(res => res.json()).then(data => {
+        const username = document.createElement("a");
+        username.innerText = data.username;
+        username.href = `/public/profile.html?id=${id}`
+
+        username.style.textDecoration = "none";
+        username.style.fontWeight = "bold";
+        username.style.fontSize = "20pt";
+
+        menu.appendChild(username);
+
+        const logout = document.createElement("div");
+        logout.style.display = "flex";
+        logout.style.flexDirection = "row";
+        logout.style.alignContent = "center";
+
+        const icon = document.createElement("img");
+        icon.src = "/public/assets/logout.png";
+        icon.style.height = "20px";
+        logout.appendChild(icon);
+        const text = document.createElement("span");
+        text.innerText = "Log Out";
+        logout.appendChild(text);
+
+        logout.onclick = (ev) => {
+            if (ev.button == 0) {
+                logOut();
+            }
+        }
+        
+        menu.appendChild(logout);
+    });
+}
+
 function commonLoad() {
-    let header = document.getElementsByTagName("header");
-
-    if (header.length != 1) {
-        console.log("ERROR: Found multiple headers!!");
-        return;
-    } else {
-        header = header[0];
-    }
-
-    const logStatusContainer = document.createElement("div");
-    logStatusContainer.id = "log-status-container";
-    header.appendChild(logStatusContainer);
-
-    const logStatus = document.createElement("div");
-    logStatus.style.margin = "10px";
-    logStatusContainer.appendChild(logStatus);
+    let profileOptions = document.getElementById("header-profile-options");
 
     id = getCookie("id");
     password = getCookie("password");
     if (!id || !password) {
-        makeLogin(logStatus);
+        makeLoggedOut(profileOptions);
     } else {
         fetch(`/api/auth?id=${id}&password=${password}`).then(res => {
             if (res.status != 200) {
@@ -98,7 +96,7 @@ function commonLoad() {
         }).then(data => {
             if (!data.correct) logOut();
 
-            makeProfile(logStatus, id, password);
+            makeLoggedIn(profileOptions, id);
         });
     }
 }
